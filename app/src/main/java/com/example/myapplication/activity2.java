c0e6466ab1dbe1d15f5cd81d4c7a209f1de18f2a
UsmanJavaid008 <bcsf18m008@pucit.edu.pk>
2021-11-11 23:51:22 +0500

    Completed

--- app/src/main/java/com/example/myapplication/activity2.java
+++ app/src/main/java/com/example/myapplication/activity2.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,125 +46,114 @@ public class activity2 extends AppCompatActivity {
 
     //0-14
     //15-32
-
-
+    String radioButtonOption = "";
+    boolean click = false;
     public void f1(View view){
         Button button10 = (Button) findViewById(R.id.nextID);
         button10.setText("RadioButton1");
+        RadioButton tempRadioButton = (RadioButton)findViewById(R.id.radioButton1);
+        String RadioButtonText = (String)tempRadioButton.getText();
+        radioButtonOption = RadioButtonText;
+        click = true;
     }
     public void f2(View view){
         Button button10 = (Button) findViewById(R.id.nextID);
         button10.setText("RadioButton2");
+        RadioButton tempRadioButton = (RadioButton)findViewById(R.id.radioButton2);
+        String RadioButtonText = (String)tempRadioButton.getText();
+        radioButtonOption = RadioButtonText;
+        click = true;
     }
     public void f3(View view){
         Button button10 = (Button) findViewById(R.id.nextID);
         button10.setText("RadioButton3");
+        RadioButton tempRadioButton = (RadioButton)findViewById(R.id.radioButton3);
+        String RadioButtonText = (String)tempRadioButton.getText();
+        radioButtonOption = RadioButtonText;
+        click = true;
     }
     public void f4(View view){
-        Button button10 = (Button) findViewById(R.id.nextID);
-        button10.setText("RadioButton4");
+        RadioButton tempRadioButton = (RadioButton)findViewById(R.id.radioButton4);
+        String RadioButtonText = (String)tempRadioButton.getText();
+        radioButtonOption = RadioButtonText;
+        click = true;
     }
 
     String[] previousAnswer = new String [4];
 
     public void NextClick(View view) {
-        counter = counter + 1;
-        if (counter>4){
-            /*Intent resultIntent = new Intent(this, ResultActivity.class);
-            resultIntent.putExtra("Result",rightAnswerCount);
-            resultIntent.putExtra("Total",33);
-            startActivity(resultIntent);*/
-            Button button10 = (Button) findViewById(R.id.nextID);
-            button10.setText("YEs");
-        }
-        else if (counter==4){
-            Button button10 = (Button) findViewById(R.id.nextID);
-            button10.setText("Finish");
-        }
-        RadioButton button1 =(RadioButton)findViewById(R.id.radioButton5);
-        RadioButton button2 =(RadioButton)findViewById(R.id.radioButton7);
-        RadioButton button3 =(RadioButton)findViewById(R.id.radioButton8);
-        RadioButton button4 =(RadioButton)findViewById(R.id.radioButton9);
-
-        TextView mcq = (TextView) findViewById(R.id.mcqText);
-
-        if(getIndexOfQuestion(randomQuestions[counter])>14){
-            for (int i=0;i<4;i++){
-                previousAnswer[i]=answers2[i];
+        if (click==true || counter==-1) {
+            counter = counter + 1;
+            if (counter > 32) {
+                Intent resultIntent = new Intent(this, ResultActivity.class);
+                resultIntent.putExtra("Result",rightAnswerCount);
+                resultIntent.putExtra("Total",33);
+                startActivity(resultIntent);
+            } else if (counter == 32) {
+                Button button10 = (Button) findViewById(R.id.nextID);
+                button10.setText("Finish");
             }
+            RadioButton button1 = (RadioButton) findViewById(R.id.radioButton1);
+            RadioButton button2 = (RadioButton) findViewById(R.id.radioButton2);
+            RadioButton button3 = (RadioButton) findViewById(R.id.radioButton3);
+            RadioButton button4 = (RadioButton) findViewById(R.id.radioButton4);
 
-        }
-        else{
-            for (int i=0;i<4;i++){
-                previousAnswer[i]=answers1[i];
-            }
-        }
-        System.out.print(previousAnswer);
-        shuffleArray(answers1);
-        shuffleArray(answers2);
+            TextView mcq = (TextView) findViewById(R.id.mcqText);
 
-        if(getIndexOfQuestion(randomQuestions[counter])>14){
-            button1.setText(answers2[0]);
-            button2.setText(answers2[1]);
-            button3.setText(answers2[2]);
-            button4.setText(answers2[3]);
-
-        }
-        else{
-            button1.setText(answers1[0]);
-            button2.setText(answers1[1]);
-            button3.setText(answers1[2]);
-            button4.setText(answers1[3]);
-
-        }
+            shuffleArray(answers1);
+            shuffleArray(answers2);
 
 
+            if(counter > 0){
+                int Index = 35;
 
-        String radioButtonOption = "";
-
-        mcq.setText(randomQuestions[counter]);
-        int Index=35;
-        if(counter!=0){
-
-            if (Index>=0 && Index<6){
-                if (radioButtonOption == "Halqiyah"){
-                    rightAnswerCount += 1;
+                if (Index >= 0 && Index < 6) {
+                    if (radioButtonOption == "Halqiyah") {
+                        rightAnswerCount += 1;
+                    }
+                } else if (Index >= 6 && Index < 8) {
+                    if (radioButtonOption == "Lahatiyah") {
+                        rightAnswerCount += 1;
+                    }
+                } else if (Index >= 9 && Index < 13) {
+                    if (radioButtonOption == "Shajariyah-Haafiyah") {
+                        rightAnswerCount += 1;
+                    }
+                } else if (Index >= 13 && Index < 16) {
+                    if (radioButtonOption == "Tarfiyah") {
+                        rightAnswerCount += 1;
+                    }
+                } else if (Index >= 16 && Index < 19) {
+                    if (radioButtonOption == "Nit-eeyah") {
+                        rightAnswerCount += 1;
+                    }
+                } else if (Index >= 19 && Index < 25) {
+                    if (radioButtonOption == "Lisaveyah") {
+                        rightAnswerCount += 1;
+                    }
+                } else if (Index >= 26 && Index < 33) {
+                    if (radioButtonOption == "Ghunna") {
+                        rightAnswerCount += 1;
+                    }
                 }
             }
-            else if (Index>=6 && Index<8){
-                if (radioButtonOption == "Lahatiyah"){
-                    rightAnswerCount += 1;
-                }
+            if (getIndexOfQuestion(randomQuestions[counter]) > 14) {
+                button1.setText(answers2[0]);
+                button2.setText(answers2[1]);
+                button3.setText(answers2[2]);
+                button4.setText(answers2[3]);
             }
-            else if (Index>=9 && Index<13){
-                if (radioButtonOption == "Shajariyah-Haafiyah"){
-                    rightAnswerCount += 1;
-                }
+            else {
+                button1.setText(answers1[0]);
+                button2.setText(answers1[1]);
+                button3.setText(answers1[2]);
+                button4.setText(answers1[3]);
             }
-            else if (Index>=13 && Index<16){
-                if (radioButtonOption == "Tarfiyah"){
-                    rightAnswerCount += 1;
-                }
-            }
-            else if (Index>=16 && Index<19){
-                if (radioButtonOption == "Nit-eeyah"){
-                    rightAnswerCount += 1;
-                }
-            }
-            else if (Index>=19 && Index<25){
-                if (radioButtonOption == "Lisaveyah"){
-                    rightAnswerCount += 1;
-                }
-            }
-            else if (Index>=26 && Index<33){
-                if (radioButtonOption == "Ghunna"){
-                    rightAnswerCount += 1;
-                }
-            }
+
+            mcq.setText(randomQuestions[counter]);
+
+            click = false;
         }
-
-
-
     }
 
     public int getIndexOfQuestion(String temp){
